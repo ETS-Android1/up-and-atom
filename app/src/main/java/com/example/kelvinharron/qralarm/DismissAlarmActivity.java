46bef04891e1d595fbf536b151e1d54e76ebe049
ctaggart13 <ctaggart13@qub.ac.uk>
2016-04-22 02:25:14 +0100

    Attempting to implement location checking in dismiss activity

--- app/src/main/java/com/example/kelvinharron/qralarm/DismissAlarmActivity.java
+++ app/src/main/java/com/example/kelvinharron/qralarm/DismissAlarmActivity.java
@@ -1,20 +1,32 @@
 package com.example.kelvinharron.qralarm;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -24,7 +36,7 @@ import java.io.IOException;
 /**
  * Created by Hannah on 12/04/2016.
  */
-public class DismissAlarmActivity extends AppCompatActivity implements OnCompleteListener {
+public class DismissAlarmActivity extends AppCompatActivity implements OnCompleteListener, LocationListener {
 
 
     //set integers required for alarm activity
@@ -38,9 +50,14 @@ public class DismissAlarmActivity extends AppCompatActivity implements OnComplet
     AlarmSQLiteHelper alarmSQLiteHelper;
     Alarm alarm;
     MediaPlayer mediaPlayer;
-
     //TODO sort out actual setting preferences to set overrideCode
     String prefOverrideCode = "1";
+
+    public static double HOME_LATITUDE = 1;
+    public static double HOME_LONGITUDE = 1;
+
+    public static double LOCATION_MARGIN = 1;
+
 
     Button snooze;
     Button scan;
@@ -49,6 +66,29 @@ public class DismissAlarmActivity extends AppCompatActivity implements OnComplet
     TextView title;
     TextView scanItem;
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean permissionDenied;
+    GoogleApiClient googleApiClient;
+    private LocationManager locationManager;
+    public Location location;
+    public double longitude;
+    public double latitude;
+    String provider;
+
+    public boolean isGPSEnabled;
+    public boolean isNetworkEnabled;
+    public boolean locationServiceAvailable;
+
+
+    //The minimum distance to change updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
+
+    //The minimum time beetwen updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 0;//1000 * 60 * 1; // 1 minute
+
+    private final static boolean forceNetwork = false;
+
+    private static DismissAlarmActivity instance = null;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,9 +125,98 @@ public class DismissAlarmActivity extends AppCompatActivity implements OnComplet
         }
 
         setupSnooze();
-        setupScan();
         setupOverride();
 
+        if (isPrefLocation()) {
+            setupScan();
+        } else {
+            setupDismiss();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            getMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true;
+        }
+    }
+
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            permissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void getMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else {
+            this.longitude = 0.0;
+            this.latitude = 0.0;
+            this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+            // Get GPS and network status
+            this.isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            this.isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (forceNetwork) isGPSEnabled = false;
+
+            if (!isNetworkEnabled && !isGPSEnabled) {
+                // cannot get location
+                this.locationServiceAvailable = false;
+            }
+            //else
+            {
+                this.locationServiceAvailable = true;
+
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        provider = LocationManager.NETWORK_PROVIDER;
+                    }
+                }//end if
+
+                if (isGPSEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        provider = LocationManager.GPS_PROVIDER;
+                    }
+                }
+            }
+        }
     }
 
     public void setupSnooze() {
@@ -108,6 +237,17 @@ public class DismissAlarmActivity extends AppCompatActivity implements OnComplet
             public void onClick(View v) {
                 String prompt = "Scan QR Code";
                 startScanning(prompt);
+            }
+        });
+    }
+
+    public void setupDismiss() {
+        scan = (Button) findViewById(R.id.scan_button);
+        scan.setText("Dismiss");
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
             }
         });
     }
@@ -170,6 +310,44 @@ public class DismissAlarmActivity extends AppCompatActivity implements OnComplet
         } else {
             Toast.makeText(getApplicationContext(), "Wrong override code, please try again.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
+    }
+
+    private boolean isPrefLocation() {
+        boolean atPrefLocation;
+        getMyLocation();
+        Location localLocation = location;
+        Location prefLocation = new Location(provider);
+        prefLocation.setLatitude(HOME_LATITUDE);
+        prefLocation.setLongitude(HOME_LONGITUDE);
+        if (localLocation.distanceTo(prefLocation) < LOCATION_MARGIN && this.locationServiceAvailable){
+            atPrefLocation = true;
+        } else {
+            atPrefLocation = false;
+        }
+        return atPrefLocation;
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
 
